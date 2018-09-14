fd2ffc1953e41ebe9bd356ea847a1576f014a6ab
Gabriel Lourenço <gaabriiel.xd@gmail.com>
2018-09-13 22:51:16 -0300

    App v.0.5 Aplicação desenvolvida das aulas 189 à 200

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms'
 import { OrdemCompraService } from '../services/ordem-compra.service'
 import { Pedido } from '../shared/pedido.model'
 
@@ -10,55 +11,26 @@ import { Pedido } from '../shared/pedido.model'
 })
 export class OrdemCompraComponent implements OnInit {
   public idPedidoCompra: number
-  /**
-    * Form com Two-way binding
-    */
-  // pristine == estado primitivo
-  public endereco: any = {value: '', valid: false, pristine: true}
-  public numero: any = {value: '', valid: false, pristine: true}
-  public complemento: any = {value: '', valid: false, pristine: true}
-  public formaPagamento: any = {value: '', valid: false, pristine: true}
+/**
+  * Formulário com template forms
+  */
+  @ViewChild('formulario') public formulario: NgForm
 
   constructor(private ordemCompraService: OrdemCompraService) { }
 
   ngOnInit() {
   }
 
-  public atualizaEndereco(endereco: string): void {
-    this.endereco.value = endereco
-    this.endereco.pristine = false
-    endereco.length > 3 ? this.endereco.valid = true : this.endereco.valid = false
-  }
-  public atualizaNumero(numero: string): void {
-    this.numero.value = numero
-    this.numero.pristine = false
-    parseInt(numero) > 0 ? this.numero.valid = true : this.numero.valid = false
-
-  }
-
-  public atualizaComplemento(comp: string): void {
-    this.complemento.value = comp
-    this.complemento.pristine = false
-    comp.length > 3 ? this.complemento.valid = true : this.complemento.valid = false
-
-  }
-
-  public atualizaFormaPagto(forma: string): void {
-    this.formaPagamento.value = forma
-    this.formaPagamento.pristine = false
-    forma != "" ? this.formaPagamento.valid = true : this.formaPagamento.valid = false
-  }
-
-  public confirmarCompra(): void {
+  public confirmaCompra(): void {
     let pedido = new Pedido(
-      this.endereco.value,
-      this.numero.value,
-      this.complemento.value,
-      this.formaPagamento.value
+      this.formulario.value.endereco,
+      this.formulario.value.numero,
+      this.formulario.value.complemento,
+      this.formulario.value.pagamento
     )
+
     this.ordemCompraService.efetivarCompra(pedido).subscribe((idPedido: number) => {
       this.idPedidoCompra = idPedido
     })
   }
-
 }
